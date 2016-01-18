9c77fbbbac1ed72e25367c4442cc374715796a69
sdygt <sdygt@users.noreply.github.com>
2016-01-18 22:08:57 +0800

    config DB

---
+++ data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'heavensky2',
+    'DB_USER' => 'root',
+    'DB_PWD' => '',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'hs2_',
+    //密钥
+    "AUTHCODE" => 'nMo4QNVFBG2NmqXZy2',
+    //cookies
+    "COOKIE_PREFIX" => 'KmeQMl_',
+);
